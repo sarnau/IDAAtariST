c2179b83f225d4a017603c59aaef24fafd747843
Markus Fritze <github@sarnau.com>
2019-04-04 21:39:53 +0200

    Adding support for TOS images

--- loaders/atari_st.py
+++ loaders/atari_st.py
@@ -4,10 +4,8 @@
 # if it exists within the file
 
 # To install: place this python script inside the 'loaders' directory
-# of IDA. Tested with IDA Pro 7.2 in macOS, but it should work on any
+# of IDA. Tested with IDA Pro 7.2 in a Mac, but it should work on any
 # platform.
-
-# 2019 Markus Fritze, <https://github.com/sarnau/IDAAtariST>
 
 import idaapi
 import ida_idp
@@ -45,8 +43,26 @@ class gemdos_executable_header(ctypes.BigEndianStructure):
 # PF_TTRAMLOAD	 1   If set, the program may be loaded into alternative RAM, otherwise it must be loaded into standard RAM.
 # PF_TTRAMMEM	 2   If set, the program's Malloc() requests may be satisfied from alternative RAM, otherwise they must be satisfied from standard RAM.
 
+# Atari TOS image header
+class tos_image_header(ctypes.BigEndianStructure):
+    _pack_ = 1
+    _fields_ = [
+        ("branch", uint16), # branch to reset handler
+        ("os_version", uint16), # OS version number
+        ("reseth", uint32), # -> system reset handler
+        ("os_beg", uint32), # -> base of OS
+        ("os_end", uint32), # -> end of OS memory usage
+        ("os_exec", uint32), # -> default shell
+        ("os_magic", uint32), # -> GEM magic (or NULL)
+        ("os_date", uint32), # date the system was built
+        ("os_conf", uint16), # configuration bits
+        ("os_dosdate", uint16), # DOS-format date the system was built
+    ]
+
+
 AtariSTBaseAddress = 0x10000 # the application is loaded to this address
 AtariSTProgramName = "Atari ST Program"
+AtariSTTOSImageName = "Atari ST TOS Image"
 
 # -----------------------------------------------------------------------
 
@@ -76,6 +92,10 @@ def accept_file(li, filename):
     header = read_struct(li, gemdos_executable_header)
     if header.PRG_magic == 0x601A:
         return {'format': AtariSTProgramName, 'processor': '68K'}
+    li.seek(0)
+    header = read_struct(li, tos_image_header)
+    if header.branch == 0x602E:
+        return {'format': AtariSTTOSImageName, 'processor': '68K'}
 
     # unrecognized format
     return 0
@@ -90,8 +110,86 @@ def load_file(li, neflags, format):
     @param neflags: options selected by the user, see loader.hpp
     @return: 0-failure, 1-ok
     """
+    if format == AtariSTTOSImageName:
+        idaapi.set_processor_type('68K', ida_idp.SETPROC_LOADER)
 
-    if format == AtariSTProgramName:
+        li.seek(0)
+        header = read_struct(li, tos_image_header)
+    
+        li.seek(0, idaapi.SEEK_END)
+        filesize = li.tell()
+
+        idc.add_segm_ex(header.os_beg, header.os_beg+filesize, 0, 1, idaapi.saRelPara, idaapi.scPub, idc.ADDSEG_NOSREG)
+        idc.set_segm_name(header.os_beg, 'TOS')
+
+        li.seek(0)
+        li.file2base(0, header.os_beg, header.os_beg+filesize, 0)
+
+        idaapi.add_entry(header.os_beg, header.os_beg, "ostext", 1)
+        idc.set_cmt(header.os_beg,'branch to reset handler', 0)
+        idc.set_name(header.os_beg+2, 'os_version')
+        idc.create_word(header.os_beg+2)
+        idc.set_cmt(header.os_beg+2,'OS version number', 0)
+        idc.set_cmt(header.os_beg+4,'-> system reset handler', 0)
+        reseth = ida_bytes.get_dword(header.os_beg+4)
+        idc.set_name(reseth, 'reseth')
+        idc.set_name(header.os_beg+8, 'os_beg')
+        idc.set_cmt(header.os_beg+8,'-> base of OS', 0)
+        idc.set_name(header.os_beg+12, 'os_end')
+        idc.set_cmt(header.os_beg+12,'-> end of OS memory usage', 0)
+        os_end = ida_bytes.get_dword(header.os_beg+12)
+        idc.add_segm_ex(0x0, os_end, 0, 1, idaapi.saRelPara, idaapi.scPub, idc.ADDSEG_NOSREG)
+        idc.set_segm_name(0x0, 'DOSRAM')
+        idc.set_name(os_end, 'endos')
+        idc.set_name(header.os_beg+16, 'os_exec')
+        idc.set_cmt(header.os_beg+16,'-> default shell', 0)
+        idc.set_name(header.os_beg+20, 'os_magic')
+        idc.set_cmt(header.os_beg+20,'-> GEM magic (or NULL)', 0)
+        idc.set_name(header.os_beg+24, 'os_date')
+        idc.create_dword(header.os_beg+24)
+        idc.set_cmt(header.os_beg+24,'date the system was built', 0)
+        idc.set_name(header.os_beg+28, 'os_conf')
+        idc.create_word(header.os_beg+28)
+        idc.set_cmt(header.os_beg+28,'configuration bits', 0)
+        idc.set_name(header.os_beg+30, 'os_dosdate')
+        idc.create_word(header.os_beg+30)
+        idc.set_cmt(header.os_beg+30,'DOS-format date the system was built', 0)
+        addr = header.os_beg+32
+        while addr < reseth:
+            if addr == header.os_beg + 0x20:
+                idc.set_cmt(addr,'base of GEMDOS pool', 0)
+                idc.set_name(ida_bytes.get_dword(addr), '_root')
+            elif addr == header.os_beg + 0x24:
+                idc.set_cmt(addr,'-> keyboard shift-state byte', 0)
+                idc.set_name(ida_bytes.get_dword(addr), 'kbshift')
+            elif addr == header.os_beg + 0x28:
+                idc.set_cmt(addr,'-> current process', 0)
+                idc.set_name(ida_bytes.get_dword(addr), '_run')
+            elif addr == header.os_beg + 0x2c:
+                idc.set_cmt(addr,'reserved for future use', 0)
+            idc.create_dword(addr)
+            addr += 4
+
+        the_magic = ida_bytes.get_dword(header.os_beg+20)
+        gem_end = ida_bytes.get_dword(the_magic+4)
+        aes_init = ida_bytes.get_dword(the_magic+8)
+        idc.add_segm_ex(os_end, gem_end+1, 0, 1, idaapi.saRelPara, idaapi.scPub, idc.ADDSEG_NOSREG)
+        idc.set_segm_name(os_end, 'GEMRAM')
+
+        idc.set_name(the_magic, 'the_magic')
+        idc.create_dword(the_magic)
+        idc.set_cmt(the_magic,'$87654321 if GEM present', 0)
+        idc.create_dword(the_magic+4)
+        idc.set_cmt(the_magic+4,'End address of OS RAM usage', 0)
+        idc.create_dword(the_magic+8)
+        idc.set_cmt(the_magic+8,'Execution address of GEM', 0)
+        idaapi.add_entry(aes_init, aes_init, "gem_entry", 1)
+        idc.set_name(gem_end, 'gem_end')
+
+        idaapi.auto_wait();
+        return 1
+
+    elif format == AtariSTProgramName:
         idaapi.set_processor_type('68K', ida_idp.SETPROC_LOADER)
 
         li.seek(0)
